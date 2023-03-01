fc6ae119ba05cd5d3cbef3d5269404629fca1c2e
aletruc <alejandrom.trucco@gmail.com>
2023-03-01 09:13:37 -0300

    Trabajo practico finalizado

---
+++ function.js
@@ -0,0 +1,7 @@
+document.getElementById('noche').addEventListener(click, function() {
+    document.body.style.backgroundColor = 'black';
+    });
+    document.getElementById('dia').addEventListener(click, function() {
+        document.body.style.backgroundColor = 'white';
+        });
+        
